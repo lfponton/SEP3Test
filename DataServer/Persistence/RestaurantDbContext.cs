7880a7ab2fe78d5714298d49f5c3b5e53230b58a
Luis <lfponton@gmail.com>
2021-11-07 16:07:16 +0100

    Implemented persistence

---
+++ DataServer/Persistence/RestaurantDbContext.cs
@@ -0,0 +1,26 @@
+﻿using DataServer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataServer.Persistence
+{
+    public class RestaurantDbContext : DbContext
+    {
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<DeliveryDriver> DeliveryDrivers { get; set; }
+        public DbSet<Menu> Menus { get; set; }
+        public DbSet<MenuItem> MenuItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Restaurant> Restaurants { get; set; }
+        public DbSet<StaffMember> StaffMembers { get; set; }
+        public DbSet<Table> Tables { get; set; }
+        public DbSet<TableBooking> TableBookings { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = C:/Users/lfpon/RiderProjects/RestaurantApp/DataServer/RestaurantDB.db");
+        }
+
+
+    }
+}
