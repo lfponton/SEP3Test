1672ec1cf738d95d70749c609b3fe71d081b7a84
Luis <lfponton@gmail.com>
2021-11-10 09:35:15 +0100

    implemented daofactory and daos

---
+++ DataServer/DataAccess/IMenuDao.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using DataServer.Models;
+
+namespace DataServer.DataAccess
+{
+    public interface IMenuDao
+    {
+        Task CreateMenuAsync(Menu menu);
+    }
+}
