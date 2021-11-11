e5aac7b1a8aeeac99983eca2647d83d4197233be
Luis <lfponton@gmail.com>
2021-11-11 09:53:35 +0100

    changes after running

--- WebClient/obj/Debug/net5.0/RazorDeclaration/Pages/TakeAway.razor.g.cs
+++ WebClient/obj/Debug/net5.0/RazorDeclaration/Pages/TakeAway.razor.g.cs
@@ -13,91 +13,91 @@ namespace WebClient.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\agosm\RiderProjects\SEP3c\WebClient\_Imports.razor"
+#line 1 "C:\Users\lfpon\RiderProjects\SEP3\WebClient\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\agosm\RiderProjects\SEP3c\WebClient\_Imports.razor"
+#line 2 "C:\Users\lfpon\RiderProjects\SEP3\WebClient\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\agosm\RiderProjects\SEP3c\WebClient\_Imports.razor"
+#line 3 "C:\Users\lfpon\RiderProjects\SEP3\WebClient\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\agosm\RiderProjects\SEP3c\WebClient\_Imports.razor"
+#line 4 "C:\Users\lfpon\RiderProjects\SEP3\WebClient\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\agosm\RiderProjects\SEP3c\WebClient\_Imports.razor"
+#line 5 "C:\Users\lfpon\RiderProjects\SEP3\WebClient\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\agosm\RiderProjects\SEP3c\WebClient\_Imports.razor"
+#line 6 "C:\Users\lfpon\RiderProjects\SEP3\WebClient\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\agosm\RiderProjects\SEP3c\WebClient\_Imports.razor"
+#line 7 "C:\Users\lfpon\RiderProjects\SEP3\WebClient\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\agosm\RiderProjects\SEP3c\WebClient\_Imports.razor"
+#line 8 "C:\Users\lfpon\RiderProjects\SEP3\WebClient\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\agosm\RiderProjects\SEP3c\WebClient\_Imports.razor"
+#line 9 "C:\Users\lfpon\RiderProjects\SEP3\WebClient\_Imports.razor"
 using WebClient;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\agosm\RiderProjects\SEP3c\WebClient\_Imports.razor"
+#line 10 "C:\Users\lfpon\RiderProjects\SEP3\WebClient\_Imports.razor"
 using Radzen;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\agosm\RiderProjects\SEP3c\WebClient\_Imports.razor"
+#line 11 "C:\Users\lfpon\RiderProjects\SEP3\WebClient\_Imports.razor"
 using Radzen.Blazor;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\agosm\RiderProjects\SEP3c\WebClient\Pages\TakeAway.razor"
+#line 2 "C:\Users\lfpon\RiderProjects\SEP3\WebClient\Pages\TakeAway.razor"
 using WebClient.Models;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\agosm\RiderProjects\SEP3c\WebClient\Pages\TakeAway.razor"
+#line 3 "C:\Users\lfpon\RiderProjects\SEP3\WebClient\Pages\TakeAway.razor"
 using WebClient.Data;
 
 #line default
@@ -112,7 +112,7 @@ using WebClient.Data;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 56 "C:\Users\agosm\RiderProjects\SEP3c\WebClient\Pages\TakeAway.razor"
+#line 56 "C:\Users\lfpon\RiderProjects\SEP3\WebClient\Pages\TakeAway.razor"
        
     private IList<Menu> menus = new List<Menu>();
     int Quantity;
