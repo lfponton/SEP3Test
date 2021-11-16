dc1cfbca9d74122560edb7003b9b9c2c344b2a2b
Marwa93mira <75796895+Marwa93mira@users.noreply.github.com>
2021-11-17 00:45:15 +0100

    Add files via upload

--- WebClient/Models/Customer.cs
+++ WebClient/Models/Customer.cs
@@ -1,11 +1,8 @@
-﻿namespace WebClient.Models
+﻿namespace Models
 {
-    public class Customer
-    {
-        public long CustomerId { get; set; }
+    public class Customer {
         public string Email { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
         public string Password { get; set; }
+        public Role Role { get; set; }
     }
 }
